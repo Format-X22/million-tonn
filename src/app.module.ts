19a57acb7af29788f80f604a9998d56dd00f5263
oPavlov <oleg.pav.m@gmail.com>
2021-05-06 02:24:41 +0300

    Configure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            cache: true,
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService): MongooseModuleOptions => ({
+                uri: configService.get<string>('MT_MONGO_CONNECT'),
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { INestApplication } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(): Promise<void> {
+    const app: INestApplication = await NestFactory.create(AppModule);
+
+    const configService: ConfigService = app.get(ConfigService);
+    const port: number = Number(configService.get<number>('MT_SERVER_PORT'));
+
+    await app.listen(port);
 }
 bootstrap();
